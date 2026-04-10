5b15e9649976cbc3532556206367c84b5e323150
Volodymyr Smoliak <svovka83@gmail.com>
2026-04-10 18:25:44 +0300

    header

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "index.html"),
-				components: resolve(__dirname, "/src/pages/components/index.html"),
+				components: resolve(__dirname, "src/pages/components/index.html"),
 			},
 		},
 	},
