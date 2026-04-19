09262b854124a0c1414fd2774dc6cded5480b6c2
Volodymyr Smoliak <svovka83@gmail.com>
2026-04-19 15:12:32 +0300

    adaptive filter grid

--- src/js/firebase/services/get-products-filter.js
+++ src/js/firebase/services/get-products-filter.js
@@ -1,4 +1,4 @@
-import { collection, getDocs, query, where, limit, onSnapshot, addDoc } from "firebase/firestore";
+import { collection, getDocs, query, orderBy, where, limit, onSnapshot, addDoc } from "firebase/firestore";
 import { productCard } from "../../components/product-card";
 import { attachFilterListeners } from "./filters/attach-filter-listeners";
 
@@ -6,23 +6,32 @@ let unsubscribe = null;
 
 export async function getFilterProducts(db) {
 	const productList = document.querySelector(".products-content__products");
+	// checkboxes
 	const checkboxHybrid = document.getElementById("hybrid");
 	const checkboxPollinated = document.getElementById("pollinated");
 	const checkboxOrganic = document.getElementById("organic");
 	const checkboxPelleted = document.getElementById("pelleted");
+	// dropdown "sort"
+	const dropdownSort = document.getElementById("filter-sort");
+	const dropdownSortList = document.getElementById("filter-sort-list");
 
 	if (!productList) return;
+	if (!dropdownSortList) return;
 
 	if (unsubscribe) unsubscribe();
 
 	const selectedTypes = [];
+	const constraints = [limit(9)];
+
+	if (dropdownSort.textContent === "Most expensive") constraints.push(orderBy("price", "desc"));
+	if (dropdownSort.textContent === "The cheapest") constraints.push(orderBy("price", "asc"));
+	if (dropdownSort.textContent === "Newly") constraints.push(orderBy("createdAt", "desc"));
+	if (dropdownSort.textContent === "Old") constraints.push(orderBy("createdAt", "asc"));
 
 	if (checkboxHybrid.checked) selectedTypes.push(checkboxHybrid.value);
 	if (checkboxPollinated.checked) selectedTypes.push(checkboxPollinated.value);
 	if (checkboxOrganic.checked) selectedTypes.push(checkboxOrganic.value);
 	if (checkboxPelleted.checked) selectedTypes.push(checkboxPelleted.value);
-
-	const constraints = [limit(9)];
 
 	if (selectedTypes.length) {
 		constraints.push(where("type", "in", selectedTypes));
@@ -49,6 +58,11 @@ export async function getFilterProducts(db) {
 
 	const checkboxes = [checkboxHybrid, checkboxPollinated, checkboxOrganic, checkboxPelleted];
 	attachFilterListeners(db, checkboxes);
+
+	dropdownSortList.addEventListener("click", () => {
+		getFilterProducts(db);
+		console.log(dropdownSort.textContent);
+	});
 }
 
 // if (hybrid.checked) constraints.push(where("type", "==", "hybrid"));
