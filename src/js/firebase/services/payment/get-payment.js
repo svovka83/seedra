0bd85004199cb208beeb1e7db34ecbe05c1ed1e5
Volodymyr Smoliak <svovka83@gmail.com>
2026-04-24 01:26:46 +0300

    payment info is done

--- src/js/firebase/services/payment/get-payment.js
+++ src/js/firebase/services/payment/get-payment.js
@@ -1,11 +1,14 @@
 import { getDoc, doc } from "firebase/firestore";
 
 export async function getPayment(db) {
-	const paymentAmount = document.getElementById("cart-summary__amount");
-	if (!paymentAmount) return;
+	const paymentAmount1 = document.getElementById("cart-summary__amount1");
+	const paymentAmount2 = document.getElementById("cart-summary__amount2");
+	if (!paymentAmount1) return;
+	if (!paymentAmount2) return;
 
 	const ref = doc(db, "total", "14");
 	const total = await getDoc(ref);
 
-	paymentAmount.innerHTML = total.data().amount / 100;
+	paymentAmount1.innerHTML = total.data().amount / 100;
+	paymentAmount2.innerHTML = total.data().amount / 100;
 }
