7e25f271d1e86f3c25d5f343b1802347920cc986
Volodymyr Smoliak <svovka83@gmail.com>
2026-05-04 06:31:36 +0300

    Updates

---
+++ assets/get-checkout-Dg1xYLjs.js
@@ -0,0 +1,9 @@
+import{d as e,f as t,n,r}from"./index.esm-CV_qY3sG.js";function i(e,t,n){return`
+  <div class="checkout-cart" id="${e}">
+    <h4 class="checkout-cart__name">${t}</h4>
+
+    <span class="checkout-cart__total">$${n/100}</span>
+
+    <hr class="checkout-cart__hr" />
+  </div>
+  `}async function a(a){let o=document.querySelector(`.checkout-carts__products`),s=document.getElementById(`cart-summary__amount`);if(!o||!s)return;s.innerHTML=(await n(t(a,`total`,`14`))).data().amount/100;let c=await r(e(a,`cart`)),l=``;c.forEach(e=>{l+=i(e.id,e.data().name,e.data().total)}),o.innerHTML=l}export{a as getCheckout};
